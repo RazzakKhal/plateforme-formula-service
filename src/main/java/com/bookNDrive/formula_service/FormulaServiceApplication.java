b1725bdd7f4c65f343681839e9ef4d874a365897
razzak <khalfallah.razzak@gmail.com>
2025-04-08 19:19:50 +0200

    first commit

---
+++ src/main/java/com/bookNDrive/formula_service/FormulaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookNDrive.formula_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookNDrive/formula_service/FormulaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookNDrive.formula_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
