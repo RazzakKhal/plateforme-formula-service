46a82359a81f649b21b5eb99861b766ea45570ca
razzak <khalfallah.razzak@gmail.com>
2025-05-13 04:10:51 +0200

    update data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS formula (
+    id IDENTITY PRIMARY KEY,
+    title VARCHAR(255),
+    description CLOB,
+    price DECIMAL(10,2),
+    code BOOLEAN,
+    promotionnal_price DECIMAL(10,2)
+);
+
 INSERT INTO formula (title, description, price, code, promotionnal_price) VALUES
 ('Forfait B 20H', 'Inscription ANTS, Accès salle de code en illimité, Code en ligne (Pass Rousseau), 20 leçons de conduite, Livret pédagogique dématérialisé, Accompagnement à l’examen pratique de la conduite', 1139.0, true, NULL),
 ('Forfait AAC 20H (Dès 15 ans)', 'Inscription ANTS, Accès salle de code en illimité, Code en ligne (Pass Rousseau), 20 leçons de conduite, Livret pédagogique dématérialisé, Accompagnement à l’examen pratique de la conduite, RDV pédagogique préalable avec l’accompagnateur (2h), 2 RDV pédagogiques (2h)', 1339.0, true, NULL),
